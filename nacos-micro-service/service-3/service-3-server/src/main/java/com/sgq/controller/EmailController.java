b8e31fbbf76c4f77ca3eb42ed60288f0f1c1d38f
sgq <shugq@foxmail.com>
2021-02-28 15:59:36 +0800

    1.实现了service3在前端的邮箱调用

---
+++ nacos-micro-service/service-3/service-3-server/src/main/java/com/sgq/controller/EmailController.java
@@ -0,0 +1,39 @@
+package com.sgq.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.sgq.api.IEmailService;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+@RestController
+public class EmailController {
+    @Reference
+    private IEmailService emailService;
+
+    @RequestMapping("/sendEmail")
+    public String sendEmail(@RequestParam String target,
+                            @RequestParam String title,
+                            @RequestParam String content){
+        ConcurrentHashMap map = new ConcurrentHashMap(){{
+            put("mail_target",target);
+            put("mail_title",title);
+            put("mail_content",content);
+        }};
+        emailService.send(map);
+        JSONObject json  = new JSONObject();
+        json.put("state","1");
+        return json.toJSONString();
+    }
+
+    @RequestMapping("/emailPage")
+    public ModelAndView emailPage(){
+        ModelAndView mv = new ModelAndView("/email/page/email");
+        return mv;
+    }
+}
